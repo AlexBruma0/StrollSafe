9dc23232a9d67ba1677100cdd644212e02243187
kevin <klc30@sfu.ca>
2022-07-04 12:39:52 -0700

    rough UI

--- AndroidStudioProjects/Group14App2/app/src/main/java/com/example/group14app/MainActivity.java
+++ AndroidStudioProjects/Group14App2/app/src/main/java/com/example/group14app/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.group14app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        configurePWD();
+        configureCaregiver();
+    }
+    public void configurePWD(){
+        Button PWD = (Button) findViewById(R.id.PWD_button);
+        PWD.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, PWD_activity.class));
+            }
+        });
+    }
+    public void configureCaregiver(){
+        Button PWD = (Button) findViewById(R.id.Caregiver);
+        PWD.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CaregiverActivity.class));
+            }
+        });
     }
 }

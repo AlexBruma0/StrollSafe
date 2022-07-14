af00aa60233f06e09d7cbb05e43cbaab0194e74b
act18 <act18@sfu.ca>
2022-07-14 00:58:17 -0700

    add listeners

--- app/src/main/java/com/example/strollsafe/pwd/PWD_signup.java
+++ app/src/main/java/com/example/strollsafe/pwd/PWD_signup.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Locale;
@@ -16,9 +17,31 @@ import java.util.Random;
 
 public class PWD_signup extends AppCompatActivity {
 
+    // back button
+    private Button btn_backToMain;
 
-    private Button btnMain;
-    private TextView txtMain;
+    // create user
+    private Button btn_createUser;
+    // PWD basic information
+    private EditText et_PWD_lastName;
+    private EditText et_PWD_firstName;
+    private EditText et_dateOfBirth;
+
+    // PWD address
+    private EditText et_addressLine1;
+    private EditText et_addressLine2;
+    private EditText et_addressCity;
+    private EditText et_addressRegion;
+    private EditText et_addressPostalCode;
+    private EditText et_addressCountry;
+
+    // Primary Caregiver emergency contact
+    private EditText et_emergencyContact_lastName;
+    private EditText et_emergencyContact_firstName;
+    private EditText et_emergencyContact_phoneNumber;
+    private EditText et_emergencyContact_email;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +49,18 @@ public class PWD_signup extends AppCompatActivity {
         setContentView(R.layout.activity_pwd_signup);
         configureBack();
 
-        btnMain = findViewById(R.id.btn_createUser);
+        btn_createUser = findViewById(R.id.btn_createUser);
 
-        btnMain.setOnClickListener(new View.OnClickListener() {
+        btn_createUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String uniqueCode = getRandomString(4);
                 Log.d("newPWDInfo", uniqueCode);
             }
         });
+        configureBack();
     } // end of onCreate()
-
+    
     private static String getRandomString(int i){
         final String characters = "abcdefghiklmnopqrstuvwxyz0123456789";
         StringBuilder result = new StringBuilder();
@@ -50,11 +74,6 @@ public class PWD_signup extends AppCompatActivity {
 
     public void configureBack() {
         Button PWD = (Button) findViewById(R.id.btn_backToMain);
-        PWD.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+        PWD.setOnClickListener(view -> finish());
     }
 }

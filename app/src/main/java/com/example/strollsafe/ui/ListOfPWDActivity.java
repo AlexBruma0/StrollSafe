8fad74a9abaee0ca5fa9ecd5f05026b0eb333d58
Alex Bruma <brumaa22@gmail.com>
2022-07-20 08:39:52 -0700

    updated list UI

--- app/src/main/java/com/example/strollsafe/ui/ListOfPWDActivity.java
+++ app/src/main/java/com/example/strollsafe/ui/ListOfPWDActivity.java
@@ -3,26 +3,33 @@ package com.example.strollsafe.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.example.strollsafe.R;
 
 public class ListOfPWDActivity extends AppCompatActivity {
+    public static final String TEXT = "text";
+    private Button button;
+    private String text;
     @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listofpwd);
         configureBack();
-        configureNewPWD();
         configureMap();
-        configureMap0();
+        configureDelete();
+        configureNew();
     }
 
     public void configureBack(){
-        ImageButton PWD = (ImageButton) findViewById(R.id.imageButton);
+        ImageButton PWD = (ImageButton) findViewById(R.id.quit);
         PWD.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,18 +38,9 @@ public class ListOfPWDActivity extends AppCompatActivity {
         });
     }
 
-    public void configureNewPWD(){
-        ImageButton PWD = (ImageButton) findViewById(R.id.imageButton2);
-        PWD.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(ListOfPWDActivity.this, PWDListActivity.class));
-            }
-        });
-    }
 
     public void configureMap(){
-        ImageButton PWD = (ImageButton) findViewById(R.id.imageButton4);
+        ImageButton PWD = (ImageButton) findViewById(R.id.Map1);
         PWD.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -51,13 +49,51 @@ public class ListOfPWDActivity extends AppCompatActivity {
         });
     }
 
-    public void configureMap0(){
-        ImageButton PWD = (ImageButton) findViewById(R.id.imageButton3);
+
+    public void configureDelete(){
+        ImageButton PWD = (ImageButton) findViewById(R.id.delete1);
+        ImageButton A = (ImageButton) findViewById(R.id.Map1);
+        Button B = (Button) findViewById(R.id.NAME1);
+
         PWD.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(ListOfPWDActivity.this, PWDLocationInformationActivity.class));
+                B.setVisibility(View.GONE);
+                PWD.setVisibility(View.GONE);
+                A.setVisibility(View.GONE);
             }
         });
+    }
+    public void configureNew(){
+        ImageButton newPWD = (ImageButton) findViewById(R.id.NewPWD);
+//        ImageButton delete = (ImageButton) findViewById(R.id.delete1);
+//        ImageButton map = (ImageButton) findViewById(R.id.Map1);
+//        Button name = (Button) findViewById(R.id.NAME1);
+        newPWD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                 startActivity(new Intent(ListOfPWDActivity.this, PWDListActivity.class));
+//                name.setVisibility(View.VISIBLE);
+//                name.setText("name");
+//                delete.setVisibility(View.VISIBLE);
+//                map.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+    public void saveData(){
+        SharedPreferences pwdPreferences = getSharedPreferences("PWD", MODE_PRIVATE);
+        SharedPreferences.Editor PreferenceEditor = pwdPreferences.edit();
+
+        PreferenceEditor.putString(TEXT,button.getText().toString());
+        PreferenceEditor.apply();
+        Toast.makeText(this, "DATA SAVED", Toast.LENGTH_SHORT).show();
+    }
+    public void loadData(){
+        SharedPreferences pwdPreferences = getSharedPreferences("PWD", MODE_PRIVATE);
+        text = pwdPreferences.getString(TEXT, "");
+
+    }
+    public void updateViews(){
+        button.setText(text);
     }
 }

49aadb7b352ec59147ee78e2a97f9f6450795100
gsstatia <gsstatia@sfu.ca>
2022-07-20 11:52:11 -0700

    added fields

--- app/src/main/java/com/example/strollsafe/ui/SettingsActivity.java
+++ app/src/main/java/com/example/strollsafe/ui/SettingsActivity.java
@@ -8,12 +8,14 @@ import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import io.realm.mongodb.User;
 
 import com.example.strollsafe.R;
 import com.example.strollsafe.utils.DatabaseManager;
 
 public class SettingsActivity extends AppCompatActivity {
     DatabaseManager databaseManager;
+    User currentUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +25,16 @@ public class SettingsActivity extends AppCompatActivity {
         setSupportActionBar(topBar);
 
         databaseManager = new DatabaseManager(this);
+        currentUser = databaseManager.isUserLoggedIn() ? databaseManager.getLoggedInUser() : null;
+        currentUser.getDeviceId();
+        currentUser.getId();
+        currentUser.getProfile().getEmail();
+        currentUser.getProfile().getFirstName();
+        currentUser.getProfile().getLastName();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.toolbar_options, menu);
         return true;

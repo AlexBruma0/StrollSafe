fe2c3bfb0d8c9e526aba84c1079f1a54ef404c48
SteveStatia <steve.statia@gmail.com>
2022-07-20 02:11:25 -0700

    Added toolbar and settings page

---
+++ app/src/main/java/com/example/strollsafe/ui/SettingsActivity.java
@@ -0,0 +1,40 @@
+package com.example.strollsafe.ui;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.example.strollsafe.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Toolbar topBar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(topBar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar_options, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.optionsMenuItem:
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}

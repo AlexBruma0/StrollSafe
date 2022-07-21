c8d7004a3eb936f063717eaf328553e6f6abb5b2
Pegah <paa40@sfu.ca>
2022-07-20 19:02:59 -0700

    login

--- app/src/main/java/com/example/strollsafe/ui/CaregiverLoginActivity.java
+++ app/src/main/java/com/example/strollsafe/ui/CaregiverLoginActivity.java
@@ -1,7 +1,9 @@
 package com.example.strollsafe.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -40,40 +42,48 @@ public class CaregiverLoginActivity extends AppCompatActivity {
         Button caregiverLoginBtn=findViewById(R.id.caregiverLoginBtn);
         String email=caregiverLoginEmail.getText().toString();
         String password=caregiverLoginPassword.getText().toString();
+        caregiverLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Credentials emailPasswordCredentials = Credentials.emailPassword(email, password);
+                    AtomicReference<User> user = new AtomicReference<User>();
+                    app.loginAsync(emailPasswordCredentials, it -> {
+                        if (it.isSuccess()) {
+                            Log.i(TAG + "asyncLoginToRealm", "Successfully authenticated using an email and password: " + email);
+                            user.set(app.currentUser());
+                            boolean isUserLoggedIn;
+                            isUserLoggedIn = true;
+                            RealmConfiguration config = new SyncConfiguration.Builder(Objects.requireNonNull(app.currentUser()), Objects.requireNonNull(app.currentUser()).getId())
+                                    .name(APP_ID)
+                                    .schemaVersion(2)
+                                    .allowQueriesOnUiThread(true)
+                                    .allowWritesOnUiThread(true)
+                                    .build();
+                            Realm.getInstanceAsync(config, new Realm.Callback() {
+                                @Override
+                                public void onSuccess(@NonNull Realm realm) {
+                                    Log.v(TAG, "Successfully opened a realm with the given config.");
+                                    realmDatabase = realm;
+                                    // CODE TO EXECUTE AFTER LOGIN
+                                    startActivity(new Intent(CaregiverLoginActivity.this, CaregiverActivity.class));
 
-
-        try {
-            Credentials emailPasswordCredentials = Credentials.emailPassword(email, password);
-            AtomicReference<User> user = new AtomicReference<User>();
-            app.loginAsync(emailPasswordCredentials, it -> {
-                if (it.isSuccess()) {
-                    Log.i(TAG + "asyncLoginToRealm", "Successfully authenticated using an email and password: " + email);
-                    user.set(app.currentUser());
-                    boolean isUserLoggedIn;
-                    isUserLoggedIn = true;
-                    RealmConfiguration config = new SyncConfiguration.Builder(Objects.requireNonNull(app.currentUser()), Objects.requireNonNull(app.currentUser()).getId())
-                            .name(APP_ID)
-                            .schemaVersion(2)
-                            .allowQueriesOnUiThread(true)
-                            .allowWritesOnUiThread(true)
-                            .build();
-                    Realm.getInstanceAsync(config, new Realm.Callback() {
-                        @Override
-                        public void onSuccess(@NonNull Realm realm) {
-                            Log.v(TAG, "Successfully opened a realm with the given config.");
-                            realmDatabase = realm;
-                            // CODE TO EXECUTE AFTER LOGIN
-
+                                }
+                            });
+                        } else {
+                            Log.e(TAG + "asyncLoginToRealm", "email: " + it.getError().toString());
+                            boolean isUserLoggedIn = false;
                         }
                     });
-                } else {
-                    Log.e(TAG + "asyncLoginToRealm", "email: " + it.getError().toString());
-                    boolean isUserLoggedIn = false;
+                } catch (Exception e) {
+                    Log.e(TAG + "asyncLoginToRealm", "" + e.getLocalizedMessage());
                 }
-            });
-        } catch (Exception e) {
-            Log.e(TAG + "asyncLoginToRealm", "" + e.getLocalizedMessage());
-        }
+
+            }
+        });
+
+
+
 
     }
 }

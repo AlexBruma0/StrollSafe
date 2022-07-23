5283ade768779f731a016eb5b48e136eb1e256c5
gsstatia <gsstatia@sfu.ca>
2022-07-23 15:59:04 -0700

    Added account info and logout button to settings page

--- app/src/main/java/com/example/strollsafe/ui/SettingsActivity.java
+++ app/src/main/java/com/example/strollsafe/ui/SettingsActivity.java
@@ -4,6 +4,10 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,9 +17,13 @@ import io.realm.mongodb.User;
 import com.example.strollsafe.R;
 import com.example.strollsafe.utils.DatabaseManager;
 
+import java.util.Objects;
+
 public class SettingsActivity extends AppCompatActivity {
     DatabaseManager databaseManager;
     User currentUser;
+    TextView loginStatusTextView, accountTypeTextView, emailTextView, userIdTextView, deviceIdTextView, firstNameTextView, lastNameTextView;
+    Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,13 +32,23 @@ public class SettingsActivity extends AppCompatActivity {
         Toolbar topBar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(topBar);
 
+        logoutButton = findViewById(R.id.logoutButton);
+        loginStatusTextView = findViewById(R.id.loginStatus);
+        accountTypeTextView = findViewById(R.id.userAccountType);
+        emailTextView = findViewById(R.id.userLoggedInEmail);
+        userIdTextView = findViewById(R.id.userId);
+        deviceIdTextView = findViewById(R.id.userDeviceId);
+        firstNameTextView = findViewById(R.id.userFirstName);
+        lastNameTextView = findViewById(R.id.userLastName);
+
         databaseManager = new DatabaseManager(this);
         currentUser = databaseManager.isUserLoggedIn() ? databaseManager.getLoggedInUser() : null;
-        currentUser.getDeviceId();
-        currentUser.getId();
-        currentUser.getProfile().getEmail();
-        currentUser.getProfile().getFirstName();
-        currentUser.getProfile().getLastName();
+        if(currentUser == null) {
+            styleSettingsPageLoggedOut();
+        } else {
+            styleSettingsPageLoggedIn();
+
+        }
     }
 
     @Override
@@ -48,5 +66,62 @@ public class SettingsActivity extends AppCompatActivity {
 
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void logoutUserFromRealm(View view) {
+        Objects.requireNonNull(databaseManager.getApp().currentUser()).logOutAsync(logoutStatus -> {
+            if(logoutStatus.isSuccess()) {
+                styleSettingsPageLoggedOut();
+                Toast.makeText(this, "Logged out.", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "" + logoutStatus.getError().toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    private void styleSettingsPageLoggedOut() {
+        final String NULL_STRING = "null";
+        loginStatusTextView.setText(String.format(getString(R.string.status_prefix), "LOGGED OUT"));
+
+        emailTextView.setText(String.format(getString(R.string.email_prefix), NULL_STRING));
+        emailTextView.setAlpha(0.5f);
+
+        userIdTextView.setText(String.format(getString(R.string.user_id_prefix), NULL_STRING));
+        userIdTextView.setAlpha(0.5f);
+
+        deviceIdTextView.setText(String.format(getString(R.string.device_id_prefix), NULL_STRING));
+        deviceIdTextView.setAlpha(0.5f);
+
+        firstNameTextView.setText(String.format(getString(R.string.first_name_prefix), NULL_STRING));
+        firstNameTextView.setAlpha(0.5f);
+
+        lastNameTextView.setText(String.format(getString(R.string.last_name_prefix), NULL_STRING));
+        lastNameTextView.setAlpha(0.5f);
+
+        logoutButton.setEnabled(false);
+        logoutButton.setAlpha(0.5f);
+    }
+
+    private void styleSettingsPageLoggedIn() {
+        loginStatusTextView.setText(String.format(getString(R.string.status_prefix), "LOGGED IN"));
+
+        emailTextView.setText(String.format(getString(R.string.email_prefix), currentUser.getProfile().getEmail()));
+        emailTextView.setAlpha(1f);
+
+        userIdTextView.setText(String.format(getString(R.string.user_id_prefix), currentUser.getId()));
+        userIdTextView.setAlpha(1.0f);
+
+        deviceIdTextView.setText(String.format(getString(R.string.device_id_prefix), currentUser.getDeviceId()));
+        deviceIdTextView.setAlpha(1f);
+
+        firstNameTextView.setText(String.format(getString(R.string.first_name_prefix), currentUser.getProfile().getFirstName()));
+        firstNameTextView.setAlpha(1f);
+
+        lastNameTextView.setText(String.format(getString(R.string.last_name_prefix), currentUser.getProfile().getLastName()));
+        lastNameTextView.setAlpha(1f);
+
+        logoutButton.setEnabled(true);
+        logoutButton.setAlpha(1f);
     }
 }

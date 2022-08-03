02914d9e19e6cd3519e7f29e25d70b7aa1eb76fd
act18 <act18@sfu.ca>
2022-08-02 18:44:31 -0700

    add comments for LocationListViewAdapter.java

--- app/src/main/java/com/example/strollsafe/ui/location/LocationListViewAdapter.java
+++ app/src/main/java/com/example/strollsafe/ui/location/LocationListViewAdapter.java
@@ -18,20 +18,39 @@ import com.example.strollsafe.pwd.PWDLocation;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 
+/**
+ * LocationListViewAdapter.java
+ *
+ * Description: Display all of the PWD's past locations in an list
+ *
+ * @since July 31, 2022
+ * @author Alvin Tsang
+ *
+ * Last updated on August 2, 2022
+ * Last updated by Alvin Tsang
+ * */
+
 @RequiresApi(api = Build.VERSION_CODES.O)
 public class LocationListViewAdapter extends RecyclerView.Adapter<LocationListViewAdapter.ViewHolder> {
 
-    // creating a variable for array list and context.
-    private ArrayList<PWDLocation> PWDLocationList;
+    private final ArrayList<PWDLocation> PWDLocationList;
     public static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
     private Context context;
 
-    // creating a constructor for our variables.
+    /**
+     * Description: Parameterized constructor
+     *
+     * @param PWDLocationList arraylist of PWD locations
+     * @param context context of which this classed is called
+     * */
     public LocationListViewAdapter(ArrayList<PWDLocation> PWDLocationList, Context context) {
         this.PWDLocationList = PWDLocationList;
         this.context = context;
-    }
+    } // end of constructor
 
+    /**
+     * Description: Implementation of interface constructor. Inflate the layout
+     * */
     @NonNull
     @Override
     public LocationListViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -39,34 +58,47 @@ public class LocationListViewAdapter extends RecyclerView.Adapter<LocationListVi
         View view = LayoutInflater.from(parent.getContext()).inflate(
                         R.layout.location_view_item, parent, false);
         return new ViewHolder(view);
-    }
+    } // end of onCreateViewHolder()
 
+    /**
+     * Description: Implementation of interface method. Set data to the views of recycler view
+     * */
     @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull LocationListViewAdapter.ViewHolder holder, int position) {
-        // setting data to our views of recycler view.
         PWDLocation location = PWDLocationList.get(position);
         holder.tv_streetAddress.setText(location.getAddress());
-        holder.tv_lastHereDateTime.setText("Last here on " + location.getLastHereDateTime().format(DATE_FORMAT));
-    }
+        holder.tv_lastHereDateTime.setText("Last here on " +
+                location.getLastHereDateTime().format(DATE_FORMAT));
+    } // end of onBindViewHolder()
 
+    /**
+     * Description: Implementation of interface method. Get the size of PWDLocationList
+     *
+     * @return size of the list
+     * */
     @Override
     public int getItemCount() {
-        // returning the size of array list.
         return PWDLocationList.size();
-    }
+    } // end of getItemCount()
 
+    /**
+     * Description: Class that describes each item in the GUI PWD list
+     * */
     public class ViewHolder extends RecyclerView.ViewHolder {
 
-        // creating variables for our views.
         private TextView tv_streetAddress;
         private TextView tv_lastHereDateTime;
 
+        /**
+         * Description: Constructor to set the text of the item field
+         * */
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             tv_streetAddress = itemView.findViewById(R.id.tv_streetAddress);
             tv_lastHereDateTime = itemView.findViewById(R.id.tv_lastHereDateTime);
-        }
+        } // end of constructor
+
     } // end of ViewHolder
 
-}
+} // end of LocationListViewAdapter.java
